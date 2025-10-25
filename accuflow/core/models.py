0d3cdd9fb21c92776b59084037e441b25fc46afb
Akhil Nasim <akhilnasim1123>
2025-10-25 21:40:03 +0530

    update

--- accuflow/core/models.py
+++ accuflow/core/models.py
@@ -86,4 +86,19 @@ class Collectors(models.Model):
     
     
 class Purchases(models.Model):
-    pass 
+    purchase_no = models.TextField(blank=True,null=True)
+    supplier = models.ForeignKey(Suppliers, on_delete=models.CASCADE, blank=True, null=True)
+    customers = models.ForeignKey(Customers, on_delete=models.CASCADE, blank=True, null=True)
+    godown = models.ForeignKey(Godowns, on_delete=models.CASCADE, blank=True, null=True)
+    date = models.DateField(blank=True,null=True)
+    code = models.TextField(blank=True,null=True)
+    qty = models.FloatField(default=0)
+    rate = models.FloatField(default=0)
+    amount = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    is_active = models.BooleanField(default=True)
+    description = models.TextField(blank=True,null=True)
+    
+    def __str__(self):
+        return self.purchase_no
+    
